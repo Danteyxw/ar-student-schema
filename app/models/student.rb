dee569e21666ed147ca0f4a29adbbdcc04420c7a
Code DIvision <codedivision@Codes-Mac-mini.local>
2015-08-19 15:55:35 +0800

    implemented 'name' and 'age' methods for Student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,21 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+    def name
+        self.first_name + " " + self.last_name
+    end
+
+    def age
+        current_date = Date.today
+        birthday = self.birthday
+
+        age = current_date.year - birthday.year
+
+        if (current_date.month < birthday.month)
+            age -= 1
+        elsif (current_date.month == birthday.month &&
+               current_date.day < birthday.date)
+        end
+        age
+    end
 end
