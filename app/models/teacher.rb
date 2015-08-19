e9aec20d65d6a9beac9d8cf2ba0782fe8df43aa7
Code DIvision <codedivision@Codes-Mac-mini.local>
2015-08-19 19:17:33 +0800

    added email validation for teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,6 +1,8 @@
 require_relative '../../db/config'
 
 class Teacher < ActiveRecord::Base
+    validates :email, uniqueness: true
+
     def name
         self.first_name + " " + self.last_name
     end
